6d5a3ac4ba188959f07173aa592fb245b953c342
Eric Stone <estone4@gmail.com>
2017-04-27 12:18:40 -0500

    Set password variable to auto.

--- Matterbot/MatterbotSample/Globals.h
+++ Matterbot/MatterbotSample/Globals.h
@@ -7,13 +7,13 @@
 namespace lospi
 {
 	std::map<Md5Digest, std::string> lookup;
-	std::string password = "7aksm7";
-	auto level = 3;
+	auto password = "s5nuq1";
+	auto level = 4;
 	auto salt_length = level+10;
 
 	void build_table(int salt_length, std::string password)
 	{
-		for (int i = 0; i <= salt_length; i++)
+		for (int i = 0; i < salt_length; i++)
 		{
 			Combinator c("hsoj", i);
 
@@ -22,7 +22,7 @@ namespace lospi
 				auto combo = c.next();
 				copy(password.begin(), password.end(), back_inserter(combo));
 				Md5Digest newhash = compute_md5(combo.data(), combo.size());
-				std::wstring md5_str = get_str_from_md5(newhash);
+				//std::wstring md5_str = get_str_from_md5(newhash);
 				lookup.emplace(newhash, std::string{ combo.begin(),combo.end() });
 			}
 		}
