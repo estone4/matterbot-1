6ca71467041c4a6ff149d8b136db92802d160b07
Eric Stone <estone4@gmail.com>
2017-04-27 14:11:54 -0500

    Working again  - level 4.  Will break if there is no match.

--- Matterbot/MatterbotSample/Challenges.h
+++ Matterbot/MatterbotSample/Challenges.h
@@ -24,49 +24,59 @@ namespace lospi
 			const std::wstring &user, const std::wstring &command_text) override {
 
 			std::string comb_input = "hsoj";
+			//auto salt_length = 11;
+			//std::string password = "7aksm7";
+			//std::map<Md5Digest, std::string> lookup;
+			std::wstring md5_str;
+			//auto result=lookup.find;
+			//Md5Digest hashinput;
 			std::vector <Md5Digest> challenge_list;
 			std::vector<std::string> answer_list;
 			std::wstringstream ss(command_text);
 			std::wstring str;
 
+			//ss = command_text;
+
 			if (lookup.empty())
 			{
-				
-				build_table(salt_length,password);
-				
+
+				build_table(salt_length, password);
+
 			}
 
 			while (std::getline(ss, str, L' '))
 			{
 				challenge_list.push_back(get_md5_from_str(str));
 			}
-			
-			// Check for hash collisions here
+
+			// do the hash collisions here
 			for (const auto& cli : challenge_list)
 			{
 				auto it = lookup.find(cli);
 				if (it != lookup.end())
 				{
-					auto answer = it->second;
+					std::string answer = it->second;
 					answer_list.push_back(answer);
 				}
 			}
 
-			// Put the answers into a string for theTry
+			// put the answers into a string for theTry
 			str = L"";
 			do
 			{
 				std::wstring answer;
-				copy(answer_list.back().begin(), answer_list.back().end(), back_inserter(answer));
+				answer = string_to_wstring(answer_list.back());
+				//copy(answer_list.back().begin(), answer_list.back().end(), back_inserter(answer));
+				//answer.append(string_to_wstring(passwd));
 				str += L" " + answer;
 				answer_list.pop_back();
 			} while (!answer_list.empty());
 
-			// Convert theAnswers to wstring and try the password list
-			auto theTry = L"rivestment try" + str;
+			// convert theAnswers to wstring and try the password list
+			std::wstring theTry = L"rivestment try" + str;
 			bot->post_message(theTry);
 			_sleep(5000);
-			auto challenge = L"rivestment challenge 100";
+			std::wstring challenge = L"rivestment challenge";
 			return challenge;
 
 		}
--- Matterbot/MatterbotSample/main.cpp
+++ Matterbot/MatterbotSample/main.cpp
@@ -1,9 +1,23 @@
 #include "Matterbot.h"
+#include <string>
 #include <iostream>
 #include <sstream>
+#include <algorithm>
+#include <fstream>
+#include <ostream>
 #include "ConsoleLogger.h"
 #include "EchoCommand.h"
+#include "WeatherCommand.h"
+#include "UpperCommand.h"
+#include "StringLengthCommand.h"
 #include "ReverseCommand.h"
+#include "LowerCommand.h"
+#include "md5Utilities.h"
+#include "Rivestment.h"
+#include "Points.h"
+#include "Scraps.h"
+#include "Globals.h"
+#include "Challenges.h"
 
 #define ERROR_SUCCESS 0
 #define ERROR_FAILURE -1
@@ -13,9 +27,9 @@ using namespace lospi;
 
 int main() {
   wstring mattermost_url = L"https://hooks.slack.com",
-    incoming_hook_route = L"services/AAAAAAA/BBBBBBBBB/CCCCCCCCCCCCCCCCCCCC",
-    outgoing_hook_route = L"http://127.0.0.1:8000/",
-    outgoing_hook_token = L"XXXXXXXXXXXXXXXXXXXXX",
+    incoming_hook_route = L"services/T4Z1QAKSS/B504GJ98D/rv1SYO7QUhl6Z7oOf7p4IqQM",
+    outgoing_hook_route = L"http://127.0.0.1:9000/",
+    outgoing_hook_token = L"yPo0ctvtdgulUMHFFmwRgmQO",
     welcome_message = L"bot is running.";
 
   try {
@@ -23,6 +37,14 @@ int main() {
     bot->set_logger(make_unique<ConsoleLogger>());
     bot->register_command(make_shared<EchoCommand>());
     bot->register_command(make_shared<ReverseCommand>(bot));
+	bot->register_command(make_shared<WeatherCommand>());
+	bot->register_command(make_shared<StringLengthCommand>(bot));
+	bot->register_command(make_shared<UpperCommand>(bot));
+	bot->register_command(make_shared<LowerCommand>(bot));
+	bot->register_command(make_shared<md5Command>(bot));
+	bot->register_command(make_shared<ChallengesCommand>(bot));
+	bot->register_command(make_shared<PointsCommand>(bot));
+	bot->register_command(make_shared<scrapsCommand>(bot));
     bot->post_message(welcome_message);
 
     wstring console;
